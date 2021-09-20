aa543c4003793b4edc0a84cda134f67b5cd9bc39
govind6498 <govindsingh6498@gmail.com>
2021-09-20 10:40:19 +0530

    Added:Interface of methods to be implemented

---
+++ src/com/bridgelabz/objectorientedprograms/InventoryOperationIF.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.objectorientedprograms;
+import com.bridgelabz.objectorientedprograms.Inventory;
+public interface InventoryOperationIF {
+	public Inventory addInventory();
+	public void calculateValue(Inventory[] inventoryArray);
+}
